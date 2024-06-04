36d9894c43c469e31f799b3a6a75e4d41079bf0f
michalBezdek <145441056+michalBezdek@users.noreply.github.com>
2024-06-04 13:13:41 +0200

    Add files via upload

---
+++ hra/script.js
@@ -0,0 +1,10 @@
+setTimeout(logoHiden , 6000)
+setTimeout(startTlacitko , 6000)
+
+function logoHiden(){
+    document.getElementById("logo").hidden = true
+}
+
+function startTlacitko(){
+    document.getElementById("start").visible = true
+}
